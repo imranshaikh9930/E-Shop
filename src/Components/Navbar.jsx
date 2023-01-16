cddb5566ac0cc154242752ca9b57a190720e45e3
Imran shaikh <imranpathan7040@gmail.com>
2023-01-16 11:40:01 +0530

    updated

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 // import"../Components/Navbar.css";
+import SearchIcon from "@mui/icons-material/Search"
 import "../App.css";
 import {Link} from "react-router-dom";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart"
@@ -8,9 +9,15 @@ import ShoppingCartIcon from "@mui/icons-material/ShoppingCart"
 function Navbar() {
   return (
     <div className="navbar">
+      <div className="searchInput">
+        <input type="text" placeholder='Search Product' />
+        <SearchIcon className='icon-search'/>
+      </div>
       <div className="links">
         <Link to="/">Shop</Link>
-        <Link to="/cart"><ShoppingCartIcon/></Link>
+        <Link to="/cart">
+          <ShoppingCartIcon />
+        </Link>
       </div>
     </div>
   )
